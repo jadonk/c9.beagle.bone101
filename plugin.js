73049a515f92a6d7e1f71e71924fd8116fb71891
Jason Kridner <jdk@ti.com>
2019-06-06 03:40:52 +0000

    Tab opens with bone101 for fixed address

--- plugin.js
+++ plugin.js
@@ -41,123 +41,33 @@ define(function(require, exports, module) {
             plugin.on("draw", function(e) {
                 console.log("bone101: draw");
                 container = e.htmlNode;
+                var tab = e.tab;
+                tab.title = "bone101";
+                tab.tooltip = "bone101";
 
-                // Insert the UI
-                ui.insertHtml(container, require("text!./plugin.bone101"), plugin);
-
-                /*
-                contents = container.querySelector(".contents");
-
-                // Set the scroll state on the session when it changes.
-                contents.addEventListener("scroll", function(){
-                    currentSession.scrollLeft = this.scrollLeft;
-                    currentSession.scrollTop = this.scrollTop;
-                });
-                */
+                var iframe = document.createElement("iframe");
+                iframe.style.width = "100%";
+                iframe.style.height = "100%";
+                iframe.style.border = 0;
+                iframe.src = "http://192.168.6.2";
+                container.appendChild(iframe);
             });
-
-            function renderOnCanvas(){}
-            function serializeValue(){}
-            function getSerializedSelection(){}
-
             plugin.on("documentLoad", function(e){
                 console.log("bone101: documentLoad");
-                var doc = e.doc;
-                var session = doc.getSession();
-
-                doc.on("setValue", function get(e) {
-                    renderOnCanvas(e.value, session.canvas);
-                }, session);
-
-                doc.on("getValue", function get(e) {
-                    return doc.changed
-                        ? serializeValue(session)
-                        : e.value;
-                }, session);
-
-                doc.tab.on("setPath", setTitle, session);
+                currentDocument = e.doc;
+                currentSession = currentDocument.getSession();
+                currentDocument.tab.on("setPath", setTitle, currentSession);
                 function setTitle(e) {
-                    doc.title = "bone101";
-                    doc.tooltip = "bone101";
+                    currentDocument.title = "bone101";
+                    currentDocument.tooltip = "bone101";
                 }
                 setTitle();
-
-                function setTheme(e) {
-                    var tab = doc.tab;
-                    var isDark = e.theme == "dark";
-
-                    tab.backgroundColor = BGCOLOR[e.theme];
-
-                    if (isDark) tab.classList.add("dark");
-                    else tab.classList.remove("dark");
-                }
-
-                layout.on("themeChange", setTheme, session);
-                setTheme({ theme: settings.get("user/general/@skin") });
-
-                // Define a way to update the state for the session
-                session.update = function(){
-                    /*
-                    contents.scrollTop = session.scrollTop;
-                    contents.scrollLeft = session.scrollLeft;
-
-                    // Set the canvas element to be displayed
-                    contents.appendChild(currentSession.canvas);
-                    */
-                };
-
-                session.canvas = document.createElement("canvas");
             });
             plugin.on("documentActivate", function(e){
                 console.log("bone101: documentActivate");
-                currentDocument = e.doc;
-                currentSession = e.doc.getSession();
-
-                /*
-                // Remove a previous canvas element, if any
-                if (contents.firstChild)
-                    contents.removeChild(contents.firstChild);
-                */
-
-                // Call the update function when this session becomes active
-                currentSession.update();
             });
             plugin.on("documentUnload", function(e){
                 console.log("bone101: documentUnload");
-                var doc = e.doc;
-                var session = doc.getSession();
-
-                if (session.canvas.parentNode)
-                    session.canvas.parentNode.removeChild(session.canvas);
-                delete session.canvas;
-            });
-            plugin.on("cut", function(e) {
-                var data = getSerializedSelection();
-                e.clipboardData.setData("text/plain", data);
-            });
-            plugin.on("getState", function(e) {
-                var session = e.doc.getSession();
-
-                e.state.scrollTop = session.scrollTop;
-                e.state.scrollLeft = session.scrollLeft;
-            });
-            plugin.on("setState", function(e) {
-                var session = e.doc.getSession();
-
-                session.scrollTop = e.state.scrollTop;
-                session.scrollLeft = e.state.scrollLeft;
-
-                if (session == currentSession)
-                    session.update();
-            });
-            plugin.on("focus", function(){
-                //contents.className = "contents focus";
-            });
-            plugin.on("blur", function(){
-                //contents.className = "contents";
-            });
-            plugin.on("resize", function(){
-                //contents.style.width = (contents.parentNode.offsetWidth - 2) + "px";
             });
             plugin.freezePublicAPI({
 
@@ -176,36 +86,26 @@ define(function(require, exports, module) {
                 }
             }, handle);
 
-            menus.addItemByPath("BeagleBone", null, 60, handle);
+            menus.addItemByPath("BeagleBone", null, 20, handle);
+            menus.addItemByPath("BeagleBone/About BeagleBone",
+                null, 22, handle);
             menus.addItemByPath("BeagleBone/bone101", new ui.item({
                 command: "bone101"
-            }), 62, handle);
+            }), 24, handle);
         });
 
         function bone101() {
             var filename = options.packagePath + ".bone101";
-            //var filename = 'http://192.168.6.2:3000/vfs/1/9cwLa0wuI72UZjWD/home/.c9/plugins/beagle.bone101/plugin.html';
-            //var filename = "http://192.168.6.2";
             console.log("opening " + filename);
             var tab = tabManager.open({
                 "path": filename,
                 "editorType": "bone101",
                 "title": "bone101",
-                "tooltip": "bone101"
+                "tooltip": "bone101",
+                "focus": true
             }, function(err, tab){
                 if (err) return console.error(err);
             });
-            //console.log("getting doc");
-            //var doc = tab.document;
-            //console.log("getting session");
-            //var session = doc.getSession();
-            //console.log("creating iframe");
-            //session.iframe = document.createElement("iframe");
-
-            /*doc.on("setValue", function get(e) {
-                console.log("setting url");
-                session.iframe.src = "//:3000";
-            }, session);*/
         }
 
         register(null, {
